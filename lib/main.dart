e7cb9c5d2a1b1ffad0cd7e881bb6ba2b52f06a6e
ippo000 <kazuho.919tnkr@gmail.com>
2025-08-14 00:04:55 +0900

    環境変数修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'pages/travel_phrases_page.dart';
 
 void main() async {
-  await dotenv.load(fileName: ".env");
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
